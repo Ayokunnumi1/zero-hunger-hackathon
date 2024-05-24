2206db812a1784a8404a5ce46f51c458208d13f0
Obimbo07 <obimboausts@gmail.com>
2024-05-24 17:11:19 +0300

    default-controller

--- app/controllers/ussd_controller.rb
+++ app/controllers/ussd_controller.rb
@@ -1,27 +1,23 @@
 class UssdController < ApplicationController
-
     skip_before_action :verify_authenticity_token
     skip_before_action :authenticate_user!
-
- 
     def handle_ussd
       session_id = params[:sessionId]
       service_code = params[:serviceCode]
       phone_number = params[:phoneNumber]
       text = params[:text]
-
+      
+      consumers = Consumer.find_by(phone_number: phone_number)
+      
+      if consumers.nil?
+        if text.empty?
+          response = "CON Welcome to Haba na Haba Food Surplus Community. Please enter your name:"
+        else
+          consumers = Consumer.create(phone_number: phone_number, name: text)
+          response = "CON Hi #{consumers.name}. welcome to Haba na Haba Food Surplus Community  \n1. Find Surplus Food \n2. Register Food Bank \n3. Be a Food Donor"
+        end
+      else
         response = case text
-
-                    consumers = Consumer.find_by(phone_number: phone_number)
-
-                    if consumers.nil?
-                      if text.empty?
-                        response = "CON Welcome to Haba na Haba Food Surplus Community. Please enter your name:"
-                      else
-                        consumers = Consumer.create(phone_number: phone_number, name: text)
-                        response = "CON Hi #{consumers.name}. welcome to Haba na Haba Food Surplus Community  \n1. Find Surplus Food \n2. Register Food Bank \n3. Be a Food Donor"
-                      end
-                    else
                    when ''
                      "CON Welcome back #{consumers.name}! \n1. Find Surplus Food \n2. Register Food Bank \n3. Be a Food Donor"
                    when '1' 
@@ -37,7 +33,7 @@ class UssdController < ApplicationController
                       "CON Here are the Food Banks near you:\n#{food_banks_list}"
                     when '1*1','1*2','1*3','1*4','1*5'
                       "CON How many people need help. \n1. 1-2 people \n2. 3-6 People \n3. 7-20 People \n4. Many(30 Approx)"
-                    when '1*1*1', '1*2*2', '1*3*3', '1*4*4'
+                    when '1*1*1'
                       "CON Choose amount of food supply needed. \n1. Small \n2. Medium \n3. Large \n4. Community"
                     when '1*1*1*1'
                       "END Thank you we are reaching out to you as soon as possible."
@@ -52,20 +48,7 @@ class UssdController < ApplicationController
                      "END Invalid choice"
                    end
       end
-    else
-      response = case text
-                 when ''
-                   "CON Welcome back #{consumers.name}! \n1. Find Surplus Food \n2. Register Food Bank \n3. Be a Food Donor"
-                 when '1'
-                   # Geolocation logic to find nearby food banks
-                   "CON Here are the Food Banks near you: \n1. Kampala \n2. Teso"
-                 when '2'
-                   "END Your phone number is #{phone_number}"
-                 else
-                   'END Invalid choice'
-                 end
+      render plain: response
     end
-
-    render plain: response
   end
-end
+  
