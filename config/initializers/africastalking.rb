ce0789ebcd51bd80426577a040bb85d284cf7b18
Ayokunnumi1 <omololuayk@gmail.com>
2024-06-30 00:58:10 +0100

    Keep key in a .env file

--- config/initializers/africastalking.rb
+++ config/initializers/africastalking.rb
@@ -1,5 +1,5 @@
 require "AfricasTalking"
 
 username = 'sandbox'
-apiKey 	= 'atsk_843bd0ad01e4517e2537bb73a798aa64e4ce85cb9d98da5bebca11a55efe57e8b470ab92'
+apiKey 	= ENV['AFRICASTALKING_API_KEY']
 @AT=AfricasTalking::Initialize.new(username, apiKey)
